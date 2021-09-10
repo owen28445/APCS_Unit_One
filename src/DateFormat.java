70f7abc4cddd548a7a2fbcac2becea4613c246e8
owen <APcs2122>
2021-09-10 11:55:06 -0400

    finished

--- src/DateFormat.java
+++ src/DateFormat.java
@@ -4,5 +4,14 @@ public class DateFormat {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.Ln):
+        System.out.println("What is the current month?");
+        String month = scan. nextLine()
+        System.out.println("What id the day of the week?");
+        String dow = scan. nextLine()
+        System.out.println("What is the current year?");
+        int year = scan . nextLine()
+        System. out.println("What is the current month?");
+        int dayofmonth = scan.nextLine()
     }
 }
