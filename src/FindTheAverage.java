bbc989f47ec7fde0e54db691960de928a707e5a5
owen <APcs2122>
2021-09-16 08:24:28 -0400

    finished

--- src/FindTheAverage.java
+++ src/FindTheAverage.java
@@ -1,9 +1,21 @@
+import java.awt.desktop.SystemEventListener;
 import java.util.Scanner;
 
 public class FindTheAverage {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the current month?");
+        String month = scan. nextLine();
+        System.out.println("What is the day of the week?");
+        String dow = scan. nextLine();
+        System.out.println("What is the current year?");
+        int year = scan . nextLine();
+        System. out.println("What is the current month?");
+        int dayofmonth = scan.nextLine();
+        System.out.println("American Format");
+        System.out.println(dow+";"+ dayofmonth+ year);
         // Create a Scanner object to get user input
 
         // Create a total variable to keep track of the total value
